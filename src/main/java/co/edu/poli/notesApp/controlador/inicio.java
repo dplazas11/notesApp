58f3945068adf213f33856ca7245c43eb5a0f8d5
dplazas <dplazas@poligran.edu.co>
2025-10-05 18:19:11 -0500

    Creacion de html para frontend

--- src/main/java/co/edu/poli/notesApp/controlador/inicio.java
+++ src/main/java/co/edu/poli/notesApp/controlador/inicio.java
@@ -2,15 +2,17 @@
 package co.edu.poli.notesApp.controlador;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class inicio {
     
-    @RequestMapping("/notesApp")
+    @GetMapping("/notesApp")
     public String inicio(){
-      return  "prueba";
+      return  "inicio";
     }
             
     
